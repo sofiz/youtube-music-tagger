5268a3464aab70bf511b680550975c0c347ef68a
sofiz <sofioussama0@gmail.com>
2020-04-11 14:07:42 +0100

    blacklist added and permission to rename

--- tagger.py
+++ tagger.py
@@ -1,18 +1,25 @@
 import os
+import re
 from mp3_tagger import MP3File
-basepath = (r'C:\Users\sofiz\Desktop\folders\test')
+
+basepath = input('music folder path :')
+rename = input("do you want to rename the songs? Y/N ")
+blacklist = ["-", ".","[" , "]" , "(", ")" , "_" , "YouTube" , "Video" , "Official", "mp3" , "Audio" , "Lyric" , "Clip"]
+
 for entry in os.listdir(basepath):
  if entry.endswith(".mp3"):
-   print (entry)
+   print(entry)
    entry_artist = entry.partition("-")[0]
+   entry_artist =  re.sub(r'|'.join(map(re.escape, blacklist)), '', entry_artist)
    print ("artist 1 : ", entry_artist)
    entry_title = entry.partition("-")[2]
-   print(entry_title)
-   if "ft" in entry_title :
+   if "ft. " in entry_title :
        artist = entry_title.partition("ft. ")[2]
+       artist =  re.sub(r'|'.join(map(re.escape, blacklist)), '', artist)
        print ("artist 2 : " , artist)
        entry_title = entry_title.partition("ft. ")[0]
-   entry_title = entry_title.partition("(" or "[" or "." or "ft")[0]
+   entry_title = entry_title.partition("(" or "[" or "." )[0]
+   entry_title =  re.sub(r'|'.join(map(re.escape, blacklist)), '', entry_title)
    print("song name : ", entry_title)
    entry_name = entry_title
    p= (basepath + "/")
@@ -21,4 +28,5 @@ for entry in os.listdir(basepath):
        audiofile.artist = (entry_artist + " / " + artist)
        audiofile.song = (entry_title)
        audiofile.save()
-       os.rename ( p + entry , p + entry_name + ".mp3")
+       if (rename=="Y") or (rename=="y"):
+           os.rename ( p + entry , p + entry_name + ".mp3")
